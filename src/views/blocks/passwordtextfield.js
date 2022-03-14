d9dc6cc20b0f84dbbda0992e068cf1d51882854e
Pavel Dzhagriev <pdzhagriev@dev.vtb>
2022-03-14 18:08:28 +0400

    pass field fix width

--- src/views/blocks/passwordtextfield.js
+++ src/views/blocks/passwordtextfield.js
@@ -2,6 +2,7 @@ import passwordtextfield from "../../assets/passwordtextfield.svg";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
+  width: 100%;
   & > input {
     pointer-events: none;
     color: ${(props) => props.textColor};
