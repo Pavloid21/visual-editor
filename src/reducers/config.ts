11a8bfb67e054921bce756fb53967c0685ebc893
Pavel Dzhagriev <pdzhagriev@dev.vtb>
2022-04-05 18:19:11 +0400

    switch reducers to typescript

--- src/reducers/config.ts
+++ src/reducers/config.ts
@@ -1,11 +1,13 @@
+import { AnyAction } from "redux";
 import actionTypes from "../constants/actionTypes";
+import { Config } from "./types";
 
-const initialState = {
+const initialState: Config = {
   activeTab: 0,
   previewMode: 3
 };
 
-export default function reducer(state = initialState, action) {
+export default function reducer(state = initialState, action: AnyAction) {
   switch (action.type) {
     case actionTypes.CHANGE_ACTIVE_TAB:
       return {
