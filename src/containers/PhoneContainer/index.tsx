e231e05ab2c7cadf82ba28a9611a1b5a95ff6d92
kuznetsov.r <roma.cuznetso2010@yandex.ru>
2022-04-15 00:10:42 +0400

    рефакторинг

--- src/containers/PhoneContainer/index.tsx
+++ src/containers/PhoneContainer/index.tsx
@@ -18,47 +18,40 @@ const PhoneContainer = (props: IPhoneContainer) => {
   const zoom: Zoom = useSelector((state: any) => state.editorMode.zoom);
 
   const zoomRef = useRef(null);
-  const wrapRef = useRef(null);
 
   useEffect(() => {
-    console.log(zoomRef);
-    console.log(zoom);
-
     // @ts-ignore
-    zoomRef.current.zoomAbs(wrapRef.current.clientWidth / 2, wrapRef.current.clientHeight / 2, Number(zoom));
+    zoomRef.current.zoomAbs(
+      // @ts-ignore
+      zoomRef.current.container.current.clientWidth / 2,
+      // @ts-ignore
+      zoomRef.current.container.current.clientHeight / 2,
+      Number(zoom)
+    );
   }, [zoom]);
 
   return (
-    <div ref={wrapRef}>
-      <PanZoom
-        ref={zoomRef}
-        autoCenter
-        disableDoubleClickZoom
-        disableKeyInteraction
-        disableScrollZoom
-        // enableBoundingBox
-      >
-        <Wrapper>
-          <div
-            style={{
-              position: 'absolute',
-              width: '428px',
-              height: '836px',
-              borderRadius: '37px',
-              border: '1px solid #000',
-              overflow: 'hidden',
-              padding: '56px 26px 0px',
-              backgroundColor: topAppBar ? topAppBar.blockId.backgroundColor : 'rgb(255, 255, 255)',
-              // clipPath: 'url(#maskRect1)',
-              ...stylesByDeviceKey[model as DeviceKeys],
-            }}
-          >
-            {props.children}
-          </div>
-        </Wrapper>
-        {mockByDeviceKey[model as DeviceKeys]}
-      </PanZoom>
-    </div>
+    <PanZoom ref={zoomRef} autoCenter disableDoubleClickZoom disableKeyInteraction disableScrollZoom>
+      <Wrapper>
+        <div
+          style={{
+            position: 'absolute',
+            width: '428px',
+            height: '836px',
+            borderRadius: '37px',
+            border: '1px solid #000',
+            overflow: 'hidden',
+            padding: '56px 26px 0px',
+            backgroundColor: topAppBar ? topAppBar.blockId.backgroundColor : 'rgb(255, 255, 255)',
+            // clipPath: 'url(#maskRect1)',
+            ...stylesByDeviceKey[model as DeviceKeys],
+          }}
+        >
+          {props.children}
+        </div>
+      </Wrapper>
+      {mockByDeviceKey[model as DeviceKeys]}
+    </PanZoom>
   );
 };
 
