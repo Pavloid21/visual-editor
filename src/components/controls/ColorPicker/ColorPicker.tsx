42609554c10d25cb1916db1ac2126aae91ab92e4
polt0s <poltos.pro@gmail.com>
2022-10-28 14:09:21 +0400

    Update component ColorPicker

--- src/components/controls/ColorPicker/ColorPicker.tsx
+++ src/components/controls/ColorPicker/ColorPicker.tsx
@@ -1,14 +1,30 @@
-import React, {ChangeEvent, useCallback, useEffect, useRef, useState} from 'react';
+import React, {
+  ChangeEvent,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+  ChangeEventHandler,
+  InputHTMLAttributes
+} from 'react';
 import {Label} from 'components/controls';
 import {Container, Wrapper} from './ColorPicker.styled';
 import {debounce} from 'external/lodash';
 import {isValidHEX, normalizeHEX} from 'utils/color';
 
-const ColorPicker: React.FC<any> = ({value, onChange, ...rest}) => {
+interface IColorPicker extends InputHTMLAttributes<HTMLInputElement> {
+  onChange: ChangeEventHandler<HTMLInputElement>;
+  label: string;
+  debouncetimeout: number;
+  $isWide: boolean;
+}
+
+const ColorPicker = ({value, onChange, ...rest}: IColorPicker) => {
   const colorRef = useRef(null);
-  const [localValue, setLocalValue] = useState<string>(value);
+  const [localValue, setLocalValue] = useState(value);
+
   useEffect(() => setLocalValue(value), [value]);
-  const doDebouncedChangeGlobalValue = useCallback(debounce(onChange, 1000), []);
+  const doDebouncedChangeGlobalValue = useCallback(debounce(onChange, 1000), [onChange]);
 
   const onBlur = (e: ChangeEvent<HTMLInputElement>) => {
     const {value: targetValue} = e.target;
@@ -30,9 +46,21 @@ const ColorPicker: React.FC<any> = ({value, onChange, ...rest}) => {
   return (
     <Wrapper>
       {rest.label && <Label>{rest.label}</Label>}
-      <Container color={value}>
-        <input ref={colorRef} type="color" value={normalizeHEX(localValue)} onChange={doChangeValue} onBlur={onBlur} />
-        <input type="text" value={localValue} onChange={doChangeValue} onBlur={onBlur} {...rest} />
+      <Container color={String(value)}>
+        <input
+          ref={colorRef}
+          type="color"
+          value={normalizeHEX(String(localValue))}
+          onChange={doChangeValue}
+          onBlur={onBlur}
+        />
+        <input
+          type="text"
+          value={localValue}
+          onChange={doChangeValue}
+          onBlur={onBlur}
+          {...rest}
+        />
       </Container>
     </Wrapper>
   );
