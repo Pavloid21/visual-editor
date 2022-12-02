2c2eb4c662ba07c487f61a4b27836a59f43d01c8
Надейкин Федор <fnadeykin@neoflex.ru>
2022-12-02 21:26:51 +0000

    Update checkbox.js

--- src/views/blocks/checkbox.js
+++ src/views/blocks/checkbox.js
@@ -4,10 +4,13 @@ import {hexToRgb} from 'constants/utils';
 import Wrapper from 'utils/wrapper';
 import checkbox_ic from 'assets/checkbox.svg';
 import {
-  backgroundColor,
-  borderColor, borderWidth, getSizeConfig,
-  imageUrl, interactive, shadowConfigBuilder,
-  shapeConfigBuilder, padding,
+  checkedIcon,
+  uncheckedIcon,
+  iconSelectedColor,
+  iconUnselectedColor,
+  padding,
+  isActive,
+  interactive,
 } from 'views/configs';
 import {blockStateSafeSelector} from 'store/selectors';
 import store, {useAppSelector} from 'store';
@@ -16,53 +19,22 @@ import {setCorrectImageUrl, getFieldValue, checkExtension} from 'utils';
 import {CustomSvg} from 'components/CustomSvg';
 
 const Image = styled.img`
+  width: 24px;
+  height: 24px;
   display: flex;
-  box-sizing: border-box;
-  background-color: ${(props) => props.backgroundColor || 'transparent'};
-  border-width: ${(props) => props.borderWidth  || 0}px;
-  border-color: ${(props) => props.borderColor || 'transparent'};
-  border-style: solid;
-  box-shadow: ${(props) => {
-    const RGB = hexToRgb(props.shadow?.color);
-    return `${props.shadow?.offsetSize?.width ?? props.shadow?.offsetSize?.widthInPercent}px ${props.shadow?.offsetSize?.height ?? props.shadow?.offsetSize?.heightInPercent
-      }px 8px rgba(${RGB?.r}, ${RGB?.g}, ${RGB?.b}, ${props.shadow?.opacity})`;
-  }};
-  object-fit: ${(props) => {
-    switch (props.imageAlignment) {
-      case 'ASPECT_FIT':
-        return 'contain';
-      case 'ASPECT_FILL':
-        return 'cover';
-      case 'SCALE_TO_FILL':
-        return 'fill';
-      default:
-        return 'contain';
-    }
-  }};
   align-self: center;
   z-index: 90;
-  ${(props) => getDimensionStyles(props)
-    .width()
-    .height()
-    .padding()
-    .apply()
-  }
-  ${(props) => {
-    if (props.shape?.type === 'ALLCORNERSROUND') {
-      return `border-radius: ${props.shape?.radius || 0}px;`;
-    }
-  }}
 `;
 
 const Component = ({settingsUI, ...props}) => {
   const {id} = useAppSelector(state => state.project);
-  const getCorrectImageUrl = setCorrectImageUrl(settingsUI.imageUrl, id);
-  const getExtension = getFieldValue(settingsUI.imageUrl);
+  const getCorrectImageUrl = setCorrectImageUrl(settingsUI.uncheckedIcon, id);
+  const getExtension = getFieldValue(settingsUI.uncheckedIcon);
 
   return (
     <Wrapper id={props.id} {...settingsUI} {...props}>
       {getExtension === 'icons' || checkExtension(getCorrectImageUrl) === 'svg' ? (
-        <CustomSvg fill={settingsUI.iconTintColor} src={getCorrectImageUrl} />
+        <CustomSvg fill={settingsUI.iconUnselectedColor} src={getCorrectImageUrl} />
       ) : (
         <Image
           {...settingsUI}
@@ -75,45 +47,69 @@ const Component = ({settingsUI, ...props}) => {
   );
 };
 
-const block = (state) => {
-  const blockState = state || blockStateSafeSelector(store.getState());
-
-  return ({
+const block = () => ({
     Component,
-    name: 'IMAGE',
+    name: 'CHECKBOX',
     title: 'Checkbox',
-    description: 'A view that displays an image.',
+    description: 'A control that checkboxes between on and off states.',
     previewImageUrl: checkbox_ic,
     category: 'Controls',
     defaultData: {
     },
     defaultInteractiveOptions: {
+      action: {url: '', fields: {}},
     },
-    interactive,
-    config: {
-      iconTintColor: {type: 'color', name: 'Icon color'},
-      imageAlignment: {
-        type: 'select',
-        name: 'Image alignment',
-        options: [
-          {label: 'Aspect fit', value: 'ASPECT_FIT'},
-          {label: 'Aspect fill', value: 'ASPECT_FILL'},
-          {label: 'Scale to fill', value: 'SCALE_TO_FILL'},
-        ],
+    interactive: {
+      field: {type: 'string', name: 'Field name'},
+      action: {
+        url: {
+          type: 'select',
+          name: 'Action URL',
+          action_types: 'actions,other',
+        },
+        method: {
+          type: 'select',
+          name: 'Method',
+          options: [
+            {label: 'Get', value: 'get'},
+            {label: 'Post', value: 'post'},
+          ],
+        },
+        fields: {
+          type: 'object',
+          properties: {
+            key: {type: 'string', name: 'Key'},
+            value: {type: 'string', name: 'Value'},
+          },
+        },
       },
-      imageUrl,
-      backgroundColor,
+      confirmationDialog: {
+        title: {
+          type: 'string',
+          name: 'Title',
+        },
+        message: {
+          type: 'string',
+          name: 'Message',
+        },
+        confirmText: {
+          type: 'string',
+          name: 'Confirm text',
+        },
+        cancelledText: {
+          type: 'string',
+          name: 'Cancelled text',
+        },
+      },
+    },
+    config: {
+      uncheckedIcon,
+      checkedIcon,
+      iconSelectedColor,
+      iconUnselectedColor,
+      isActive,
       padding,
-      borderWidth,
-      borderColor,
-      size: getSizeConfig(blockState.deviceInfo.device),
-      shape: shapeConfigBuilder()
-        .withRadius
-        .withAllCornersRound
-        .done(),
-      shadow: shadowConfigBuilder().done(),
     },
   });
-};
 
 export default block;
