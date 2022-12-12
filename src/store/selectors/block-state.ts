758e81d2038232fd170219112aee813c6b53506a
polt0s <poltos.pro@gmail.com>
2022-12-12 22:24:16 +0400

    fix/NFWHITELABELMOB-500 Fix component collection

--- src/store/selectors/block-state.ts
+++ src/store/selectors/block-state.ts
@@ -4,12 +4,26 @@ import {BlocksState} from 'views/blocks/types';
 import {Device} from 'containers/MobileSelect/consts';
 
 const selectSelf = (state: RootStore) => state;
+const getBlocksSelector = (state: RootStore) => state.layout.blocks;
 
 const getBlockState = ({editorMode}: RootStore): BlocksState => ({
   deviceInfo: {
     device: editorMode.device as Device,
     model: editorMode.model,
     dpi: editorMode.dpi
+  }
+});
+
+export const getListItemCollectionSelector = createSelector(getBlocksSelector, (blocks) => {
+  const firstIndex = 0;
+
+  if(blocks.length) {
+    const checkBlocksLength =  blocks[firstIndex].listItems.map((item) => {
+      if(item.blockId === 'collection' && item.listItem !== null) {
+        return item.listItem;
+      }
+    });
+    return checkBlocksLength.filter((item) => item !== undefined);
   }
 });
 
